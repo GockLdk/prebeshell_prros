5fe4d225dc9698247d87b0b8369d32f5bdd50f2d
Mario <marioalverto543@hotmail.com>
2018-09-16 21:52:21 -0500

    manualito junto con arbol

---
+++ shellprebe/arbolito.sh
@@ -0,0 +1,19 @@
+clear
+	echo -E '.'
+	echo -E '├── arbolito.sh'
+	echo -E '├── ayuda.1'
+	echo -E '├── bienvenido.txt'
+	echo -E '├── buscar.sh'
+	echo -E '├── creditos.txt'
+    echo -E '├── ejemplo.txt'
+	echo -E '├── fecha.sh'
+	echo -E '├── gato1.sh'
+	echo -E '├── hora.sh'
+	echo -E '├── infos.sh'
+	echo -E '├── main.sh'
+	echo -E '├── menujuegos.sh'
+    echo -E '├── piedra.sh'
+	echo -E '├── player.sh'
+	echo -E '└── tux.txt'	
+	
+	echo -e "\n0 directorios, 15 archivos\n"
